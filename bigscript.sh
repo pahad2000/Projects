80e30ecaf18326471ea94cfeee6d806cc7d33355
pahad2000 <43926244+pahad2000@users.noreply.github.com>
2020-05-25 18:23:42 +1000

    Delete bigscript.sh

--- bigscript.sh
+++
@@ -1,8 +0,0 @@
-xterm -hold -title "Peer 1" -e "`pwd`/cdht 1 3 4 300 0.3" &
-xterm -hold -title "Peer 3" -e "`pwd`/cdht 3 4 5 300 0.3" &
-xterm -hold -title "Peer 4" -e "`pwd`/cdht 4 5 8 300 0.3" &
-xterm -hold -title "Peer 5" -e "`pwd`/cdht 5 8 10 300 0.3" &
-xterm -hold -title "Peer 8" -e "`pwd`/cdht 8 10 12 300 0.3" &
-xterm -hold -title "Peer 10" -e "`pwd`/cdht 10 12 15 300 0.3" &
-xterm -hold -title "Peer 12" -e "`pwd`/cdht 12 15 1 300 0.3" &
-xterm -hold -title "Peer 15" -e "`pwd`/cdht 15 1 3 300 0.3" &
