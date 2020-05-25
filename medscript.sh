6737c6cb485b1ac17f6cdd87d971d0e005700d7c
pahad2000 <43926244+pahad2000@users.noreply.github.com>
2020-05-25 18:24:19 +1000

    Delete medscript.sh

--- medscript.sh
+++
@@ -1,4 +0,0 @@
-xterm -hold -title "Peer 1" -e "`pwd`/cdht 1 3 4 300 0.3" &
-xterm -hold -title "Peer 3" -e "`pwd`/cdht 3 4 5 300 0.3" &
-xterm -hold -title "Peer 4" -e "`pwd`/cdht 4 5 1 300 0.3" &
-xterm -hold -title "Peer 5" -e "`pwd`/cdht 5 1 3 300 0.3" &
